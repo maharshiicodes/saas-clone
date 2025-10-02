8c629d37c361464690f1c49730f06fffed24cde8
Maharshi <maharshilavana@gmail.com>
2025-10-02 23:40:08 +0530

    'implemented infinite scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import NavBar from './components/NavBar';
 import Home from './components/Home';
-import {Routes , Route} from 'react-router-dom'
+import InfiniteScroll from './components/InfiniteScroll';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    <div className="bg-black">
       <NavBar />
       <main>
         <Routes>
           <Route path="/" element={<Home />} /> 
         </Routes>
       </main>
-    </>
-  )
+      <InfiniteScroll />
+    </div>
+  );
 }
 
 export default App;
